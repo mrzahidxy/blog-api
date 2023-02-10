feecaab4dea9ecc16bfa652d7a32587d291ae4ee
Zahid <mrzahidxy@gmail.com>
2023-02-10 22:09:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRouter = require("./Routes/auth");
+
+dotenv.config();
+
+const app = express();
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB is connected"))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors());
+
+//ROUTING
+app.use("/api/auth", authRouter);
+
+app.listen(5000, () => {
+  console.log("BACKEND IS RUNNING");
+});
